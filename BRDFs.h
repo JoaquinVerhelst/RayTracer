63e203053823745efb1f52612e646092eda4cf30
joquillan <100207832+joquillan@users.noreply.github.com>
2022-11-11 11:48:38 +0100

    Add files via upload

---
+++ BRDFs.h
@@ -0,0 +1,118 @@
+#pragma once
+#include <cassert>
+#include "Math.h"
+
+namespace dae
+{
+	namespace BRDF
+	{
+		/**
+		 * \param kd Diffuse Reflection Coefficient
+		 * \param cd Diffuse Color
+		 * \return Lambert Diffuse Color
+		 */
+		static ColorRGB Lambert(float kd, const ColorRGB& cd)
+		{
+			return (kd * cd) / M_PI;
+		}
+
+		static ColorRGB Lambert(const ColorRGB& kd, const ColorRGB& cd)
+		{
+			return (kd * cd) / M_PI;
+		}
+
+		/**
+		 * \brief todo
+		 * \param ks Specular Reflection Coefficient
+		 * \param exp Phong Exponent
+		 * \param l Incoming (incident) Light Direction
+		 * \param v View Direction
+		 * \param n Normal of the Surface
+		 * \return Phong Specular Color
+		 */
+		static ColorRGB Phong(float ks, float exp, const Vector3& l, const Vector3& v, const Vector3& n)
+		{
+			ColorRGB phong = {};
+			Vector3 reflect = Vector3::Reflect(l, n);
+			float cosA = std::max(Vector3::Dot(reflect, v), 0.0f);
+			if (cosA > 0.f)
+			{
+				phong +=  ColorRGB{ 1.f, 1.f, 1.f } * ks * powf(cosA, exp);
+				return  phong;
+			}
+			return phong;
+		}
+
+		/**
+		 * \brief BRDF Fresnel Function >> Schlick
+		 * \ ( param h Normalized Halfvector between View and Light directions )
+		 * 
+		 * \param h is replaced with the surfaces normal param n
+		 * 
+		 * \param v Normalized View direction
+		 * \param f0 Base reflectivity of a surface based on IOR (Indices Of Refrection), this is different for Dielectrics (Non-Metal) and Conductors (Metal)
+		 * \return
+		 */
+		static ColorRGB FresnelFunction_Schlick(const Vector3& n, const Vector3& v, const ColorRGB& f0)
+		{
+			ColorRGB F = f0 + (ColorRGB{ 1.0f, 1.0f, 1.0f } - f0) * powf(1.f - Vector3::Dot(n , v), 5);
+
+			return F;
+		}
+
+		/**
+		 * \brief BRDF NormalDistribution >> Trowbridge-Reitz GGX (UE4 implemetation - squared(roughness))
+		 * \param n Surface normal
+		 * \param h Normalized half vector
+		 * \param roughness Roughness of the material
+		 * \return BRDF Normal Distribution Term using Trowbridge-Reitz GGX
+		 */
+		static float NormalDistribution_GGX(const Vector3& n, const Vector3& h, float roughness)
+		{
+			float a = roughness * roughness;
+			float a2 = a * a;
+			float NdotH = std::max(Vector3::Dot(n, h), 0.0f);
+			float NdotH2 = NdotH * NdotH;
+
+			float nom = a2;
+			float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+			denom = M_PI * denom * denom;
+
+			return nom / denom;
+
+		}
+
+
+		/**
+		 * \brief BRDF Geometry Function >> Schlick GGX (Direct Lighting + UE4 implementation - squared(roughness))
+		 * \param n Normal of the surface
+		 * \param v Normalized view direction
+		 * \param k roughness  remapped based on direct lightning
+		 * \return BRDF Geometry Term using SchlickGGX
+		 */
+		static float GeometryFunction_SchlickGGX(const Vector3& n, const Vector3& v, float k)
+		{
+			float G = Vector3::Dot(n , v) / (Vector3::Dot(n , v) * (1.f - k) + k);
+			return G;
+		}
+
+		/**
+		 * \brief BRDF Geometry Function >> Smith (Direct Lighting)
+		 * \param n Normal of the surface
+		 * \param v Normalized view direction
+		 * \param l Normalized light direction
+		 * \param roughness Roughness of the material
+		 * \param k roughness  remapped based on direct lightning
+		 * \return BRDF Geometry Term using Smith (> SchlickGGX(n,v,roughness) * SchlickGGX(n,l,roughness))
+		 */
+		static float GeometryFunction_Smith(const Vector3& n, const Vector3& v, const Vector3& l, float roughness)
+		{
+			float a = roughness * roughness;
+			float k = ((a + 1.0f) * (a + 1.0f)) / 8;
+
+			float Gsmith = GeometryFunction_SchlickGGX(n, -v, k) * GeometryFunction_SchlickGGX(n, l, k);
+			return Gsmith;
+		}
+
+	}
+}
---
+++ Utils.h
@@ -0,0 +1,318 @@
+#pragma once
+#include <cassert>
+#include <fstream>
+#include "Math.h"
+#include "DataTypes.h"
+#include <cmath>
+
+
+namespace dae
+{
+	namespace GeometryUtils
+	{
+#pragma region Sphere HitTest
+		//SPHERE HIT-TESTS
+		inline bool HitTest_Sphere(const Sphere& sphere, const Ray& ray, HitRecord& hitRecord, bool ignoreHitRecord = false)
+		{
+
+			float t0{}, t1{};
+
+			float a = Vector3::Dot(ray.direction, ray.direction);
+			float b = Vector3::Dot(2 * ray.direction, ray.origin - sphere.origin);
+			float c = (Vector3::Dot(ray.origin - sphere.origin, ray.origin - sphere.origin)) - (sphere.radius * sphere.radius);
+
+			float discriminant = b * b - 4 * a * c;
+
+			if (discriminant < 0)
+				return false;
+			else if (discriminant == 0)
+				t0 = -0.5 * b / a;
+			else
+			{
+				float sqrtDiscriminant = sqrtf(discriminant);
+				float q = (b > 0) ? -0.5 * (b + sqrtDiscriminant) : -0.5 * (b - sqrtDiscriminant);
+
+				t0 = q / a;
+				t1 = c / q;
+			}
+
+
+			if (t0 > t1)
+				std::swap(t0, t1);
+
+			if (t0 < ray.min || t0 > ray.max)
+			{
+				t0 = t1;
+				if (t0 < ray.min || t0 > ray.max)
+					return false;
+			}
+
+			if (!ignoreHitRecord)
+			{
+				hitRecord.t = t0;
+				hitRecord.didHit = true;
+				hitRecord.materialIndex = sphere.materialIndex;
+				hitRecord.origin = ray.origin + t0 * ray.direction;
+				hitRecord.normal = ((ray.origin - sphere.origin) + (t0 * ray.direction)) / sphere.radius;
+			}
+
+			return true;
+
+		}
+
+		inline bool HitTest_Sphere(const Sphere& sphere, const Ray& ray)
+		{
+			HitRecord temp{};
+			return HitTest_Sphere(sphere, ray, temp, true);
+		}
+#pragma endregion
+#pragma region Plane HitTest
+		//PLANE HIT-TESTS
+		inline bool HitTest_Plane(const Plane& plane, const Ray& ray, HitRecord& hitRecord, bool ignoreHitRecord = false)
+		{
+
+			float denominator = Vector3::Dot(ray.direction, plane.normal);
+
+			if (fabs(denominator) > 0.00001f)
+			{
+				float t = Vector3::Dot(plane.origin - ray.origin, plane.normal) / denominator;
+
+				if (t > ray.min && t < ray.max)
+				{
+					if (!ignoreHitRecord)
+					{
+						hitRecord.t = t;
+						hitRecord.didHit = true;
+						hitRecord.materialIndex = plane.materialIndex;
+						hitRecord.origin = ray.origin + t * ray.direction;
+						hitRecord.normal = plane.normal;
+					}
+					return true;
+				}
+				else
+					return false;
+				
+			}
+		}
+
+		inline bool HitTest_Plane(const Plane& plane, const Ray& ray)
+		{
+			HitRecord temp{};
+			return HitTest_Plane(plane, ray, temp, true);
+		}
+#pragma endregion
+#pragma region Triangle HitTest
+		//TRIANGLE HIT-TESTS
+		inline bool HitTest_Triangle(const Triangle& triangle, const Ray& ray, HitRecord& hitRecord, bool ignoreHitRecord = false)
+		{
+			Vector3 normal = triangle.normal;
+
+
+			float NoRaydir = Vector3::Dot(normal, ray.direction);
+
+			if (NoRaydir == 0)
+				return false;
+
+
+			Vector3 center = { (triangle.v0 + triangle.v1 + triangle.v2) / 3 };
+			Vector3 L = center - ray.origin;
+
+			float t = Vector3::Dot(L, normal) / NoRaydir;
+
+
+			if (t < ray.min || t > ray.max) 
+				return false;
+
+			Vector3 P = ray.origin + t * ray.direction;
+
+
+			Vector3 edgeA = triangle.v1 - triangle.v0;
+			Vector3 pointToSide = P - triangle.v0;
+			if (Vector3::Dot(normal, Vector3::Cross(edgeA, pointToSide)) < 0)
+				return false;
+
+			Vector3 edgeB = triangle.v2 - triangle.v1;
+			pointToSide = P - triangle.v1;
+			if (Vector3::Dot(normal, Vector3::Cross(edgeB, pointToSide)) < 0)
+				return false;
+
+			Vector3 edgeC = triangle.v0 - triangle.v2;
+			pointToSide = P - triangle.v2;
+			if (Vector3::Dot(normal, Vector3::Cross(edgeC, pointToSide)) < 0)
+				return false;
+
+
+			//Cullmode
+
+
+			switch (triangle.cullMode)
+			{
+			case TriangleCullMode::BackFaceCulling:
+				if (Vector3::Dot(triangle.normal, ray.direction) > 0)
+					return false;
+				break;
+			case TriangleCullMode::FrontFaceCulling:
+				if (Vector3::Dot(triangle.normal, ray.direction) < 0)
+					return false;
+				break;
+			case TriangleCullMode::NoCulling:
+				break;
+			}
+
+
+			if (!ignoreHitRecord)
+			{
+				hitRecord.t = t;
+				hitRecord.didHit = true;
+				hitRecord.materialIndex = triangle.materialIndex;
+				hitRecord.origin = P;
+				hitRecord.normal = triangle.normal;
+			}
+
+			return true;
+		}
+
+		inline bool HitTest_Triangle(const Triangle& triangle, const Ray& ray)
+		{
+			HitRecord temp{};
+			return HitTest_Triangle(triangle, ray, temp, true);
+		}
+#pragma endregion
+#pragma region TriangeMesh HitTest
+		inline bool HitTest_TriangleMesh(const TriangleMesh& mesh, const Ray& ray, HitRecord& hitRecord, bool ignoreHitRecord = false)
+		{
+			auto pos = mesh.transformedPositions;
+			auto indices = mesh.indices;
+			auto normals = mesh.transformedNormals;
+
+			HitRecord tempRecord;
+
+			for (size_t i = 0; i < indices.size(); i += 3)
+			{
+				Triangle tempTriangle = { pos[indices[i]], pos[indices[i + 1]] , pos[indices[i + 2]], normals[i]};
+
+				if (HitTest_Triangle(tempTriangle, ray, hitRecord))
+				{
+					if (!tempRecord.didHit)
+						tempRecord = hitRecord;
+					else if (tempRecord.t < hitRecord.t)
+						hitRecord = tempRecord;
+					else
+						tempRecord = hitRecord;
+
+				}
+			}
+			if (!tempRecord.didHit)
+				return false;
+
+			hitRecord = tempRecord;
+			return true;
+
+		}
+
+		inline bool HitTest_TriangleMesh(const TriangleMesh& mesh, const Ray& ray)
+		{
+			HitRecord temp{};
+			return HitTest_TriangleMesh(mesh, ray, temp, true);
+		}
+#pragma endregion
+	}
+
+	namespace LightUtils
+	{
+		//Direction from target to light
+		inline Vector3 GetDirectionToLight(const Light& light, const Vector3 origin)
+		{
+			return light.origin - origin;
+		}
+
+		inline ColorRGB GetRadiance(const Light& light, const Vector3& target)
+		{
+			ColorRGB Ergb = {};
+
+			if (light.type == LightType::Point)
+				Ergb = light.color * (light.intensity / (light.origin - target).SqrMagnitude());
+			else if (light.type == LightType::Directional)
+				Ergb = light.color * light.intensity;
+			
+			return Ergb;
+
+		}
+	}
+
+	namespace Utils
+	{
+		//Just parses vertices and indices
+#pragma warning(push)
+#pragma warning(disable : 4505) //Warning unreferenced local function
+		static bool ParseOBJ(const std::string& filename, std::vector<Vector3>& positions, std::vector<Vector3>& normals, std::vector<int>& indices)
+		{
+			std::ifstream file(filename);
+			if (!file)
+				return false;
+
+			std::string sCommand;
+			// start a while iteration ending when the end of file is reached (ios::eof)
+			while (!file.eof())
+			{
+				//read the first word of the string, use the >> operator (istream::operator>>) 
+				file >> sCommand;
+				//use conditional statements to process the different commands	
+				if (sCommand == "#")
+				{
+					// Ignore Comment
+				}
+				else if (sCommand == "v")
+				{
+					//Vertex
+					float x, y, z;
+					file >> x >> y >> z;
+					positions.push_back({ x, y, z });
+				}
+				else if (sCommand == "f")
+				{
+					float i0, i1, i2;
+					file >> i0 >> i1 >> i2;
+
+					indices.push_back((int)i0 - 1);
+					indices.push_back((int)i1 - 1);
+					indices.push_back((int)i2 - 1);
+				}
+				//read till end of line and ignore all remaining chars
+				file.ignore(1000, '\n');
+
+				if (file.eof()) 
+					break;
+			}
+
+			//Precompute normals
+			for (uint64_t index = 0; index < indices.size(); index += 3)
+			{
+				uint32_t i0 = indices[index];
+				uint32_t i1 = indices[index + 1];
+				uint32_t i2 = indices[index + 2];
+
+				Vector3 edgeV0V1 = positions[i1] - positions[i0];
+				Vector3 edgeV0V2 = positions[i2] - positions[i0];
+				Vector3 normal = Vector3::Cross(edgeV0V1, edgeV0V2);
+
+				if(isnan(normal.x))
+				{
+					int k = 0;
+				}
+
+				normal.Normalize();
+				if (isnan(normal.x))
+				{
+					int k = 0;
+				}
+
+				normals.push_back(normal);
+			}
+
+			return true;
+		}
+#pragma warning(pop)
+	}
+}
+
